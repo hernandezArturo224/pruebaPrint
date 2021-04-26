91c5c709657789b5068c564061bd1047a6ea77d0
ArturoHdez <hernandezarturo224@gmail.com>
2021-04-26 16:43:54 +0200

    Todo el proceso hecho... falta probar y ver fallos?

--- app/src/main/java/com/example/pruebaprint/escaner.java
+++ app/src/main/java/com/example/pruebaprint/escaner.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
+import com.hp.mobile.scan.sdk.ScanCapture;
+import com.hp.mobile.scan.sdk.ScanTicketValidator;
 import com.hp.mobile.scan.sdk.Scanner;
 import com.hp.mobile.scan.sdk.ScannerCapabilitiesFetcher;
 import com.hp.mobile.scan.sdk.ScannerException;
 import com.hp.mobile.scan.sdk.browsing.ScannersBrowser;
+import com.hp.mobile.scan.sdk.model.Resolution;
+import com.hp.mobile.scan.sdk.model.ScanPage;
+import com.hp.mobile.scan.sdk.model.ScanTicket;
 import com.hp.mobile.scan.sdk.model.ScannerCapabilities;
 
 import java.util.ArrayList;
@@ -20,6 +26,9 @@ public class escaner extends AppCompatActivity {
 
     private List<Scanner> mScanners;
     ScannersBrowser scannerBrowser;
+    Scanner scaner;
+    ScanTicket ticket;
+    String path;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +54,7 @@ public class escaner extends AppCompatActivity {
     }
 
     public void putCapabilities(){
-        Scanner scaner = mScanners.get(0);
+        scaner = mScanners.get(0);
         scaner.fetchCapabilities(new ScannerCapabilitiesFetcher.ScannerCapabilitiesListener() {
             @Override
             public void onFetchCapabilities(ScannerCapabilities scannerCapabilities) {
@@ -60,7 +69,39 @@ public class escaner extends AppCompatActivity {
     }
 
     public void setTicket(){
+        ticket = ScanTicket.createWithPreset(ScanTicket.SCAN_PRESET_PHOTO);
+        ticket.setSetting(ScanTicket.SCAN_SETTING_RESOLUTION,new Resolution(1200,1200));
+        scaner.validateTicket(ticket, new ScanTicketValidator.ScanTicketValidationListener() {
+            @Override
+            public void onScanTicketValidationComplete(ScanTicket scanTicket) {
 
+            }
+
+            @Override
+            public void onScanTicketValidationError(ScannerException e) {
+
+            }
+        });
+    }
+
+    public void startScanning(){
+        //path = Common.getAppPath(escaner.this)+"prueba"+ticket.get;
+        scaner.scan(path, ticket, new ScanCapture.ScanningProgressListener() {
+            @Override
+            public void onScanningPageDone(ScanPage scanPage) {
+
+            }
+
+            @Override
+            public void onScanningError(ScannerException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onScanningComplete() {
+                Toast.makeText(escaner.this,"Scan succesful",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
